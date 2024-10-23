d545cec7463f48bc633c2e477cf2c030a7faa30b
Adam Kaspar <wakaspar@gmail.com>
2024-10-23 13:33:45 -0400

    updates draggable js

--- src/lib/draggable.js
+++ src/lib/draggable.js
@@ -1,13 +1,17 @@
 // From: https://www.w3schools.com/howto/howto_js_draggable.asp
-export function draggable(id) {
+export function draggable(id, startX = 0, startY = 0) {
     dragElement(document.getElementById(id)); // makes element draggable
 
-    function dragElement(elmnt) {
+    function dragElement(element) {
         var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
+        // TODO: makes these responsive ??
+        element.style.top = startY + 'px';
+        element.style.left = startX + 'px';
+
         // if present, move div from header. otherwise, move div from anywhere inside.
-        var headerId = document.getElementById(elmnt.id + '-header')
-        headerId ? headerId.onmousedown = dragMouseDown : elmnt.onmousedown = dragMouseDown;
+        var headerId = document.getElementById(element.id + '-header')
+        headerId ? headerId.onmousedown = dragMouseDown : element.onmousedown = dragMouseDown;
 
         function dragMouseDown(e) {
             e = e || window.addEventListener();
@@ -29,8 +33,8 @@ export function draggable(id) {
             pos3 = e.clientX;
             pos4 = e.clientY;
             // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + 'px';
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px';
+            element.style.top = (element.offsetTop - pos2) + 'px';
+            element.style.left = (element.offsetLeft - pos1) + 'px';
         }
 
         function closeDragElement() {
